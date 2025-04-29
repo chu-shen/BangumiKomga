4f8d3002334d07ac514c70747075b2222788d61e
wiige@live.com <wiige@live.com>
2025-04-30 00:20:27 +0800

    顺手清理TODO

--- refreshMetadataServive.py
+++ refreshMetadataServive.py
@@ -14,8 +14,6 @@ class PollingCaller:
 
     def start_polling(self):
         """启动服务"""
-        # TODO: 添加 refresh_metadata() 是否正在执行的判断
-        # TODO: 添加 refresh_metadata() 正在执行时的等待
         def poll():
             while True:
                 try:

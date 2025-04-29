193a64de51a1d83fa17bbe6fed79826ffb514f60
初神 <45256288+chu-shen@users.noreply.github.com>
2025-04-29 13:56:19 +0800

    docs: 修正「为小说添加元数据」的说明

--- tools/resortSearchResultsList.py
+++ tools/resortSearchResultsList.py
@@ -30,19 +30,16 @@ def resort_search_list(query, results, threshold, DataSource):
         manga_metadata = DataSource.get_subject_metadata(manga_id)
         if not manga_metadata:
             continue
-        # 根据 IS_NOVEL_ONLY 配置判断是否只应用于 Komga 的小说库
-        if IS_NOVEL_ONLY:
-            novel_filter = SubjectPlatform.parse(
-                manga_metadata["platform"]) == SubjectPlatform.Novel
-        else:
-            novel_filter = SubjectPlatform.parse(
-                manga_metadata["platform"]) != SubjectPlatform.Novel
-        # bangumi书籍类型包括：漫画、小说、画集、其他
-        # 由于komga不支持小说文字的读取，这里直接忽略`小说`类型，避免返回错误结果
         # bangumi书籍系列包括：系列、单行本
         # 此处需去除漫画系列的单行本，避免干扰，官方 API 已添加 series 字段（是否系列，仅对书籍类型的条目有效）
         # bangumi数据中存在单行本与系列未建立联系的情况
-        if novel_filter and manga_metadata["series"]:
+        if not manga_metadata["series"]:
+            continue
+        # bangumi书籍类型包括：漫画、小说、画集、其他
+        platform=SubjectPlatform.parse(manga_metadata["platform"])
+        # 根据 IS_NOVEL_ONLY 配置判断是否只应用于 Komga 的小说库
+        is_target_platform = (platform == SubjectPlatform.Novel) == IS_NOVEL_ONLY
+        if is_target_platform:
             # 计算得分
             score = compute_name_score_by_fuzzy(
                 manga_metadata["name"],

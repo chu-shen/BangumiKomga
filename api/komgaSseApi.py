419e3875aac56233754e9516d8222ed5d48db406
wiige@live.com <wiige@live.com>
2025-05-20 14:57:36 +0800

    fix: 更改`on_event`行为, 加入回调函数注册与通知

--- api/komgaSseApi.py
+++ api/komgaSseApi.py
@@ -5,14 +5,15 @@
 # ------------------------------------------------------------------
 
 
-from config.config import KOMGA_BASE_URL, KOMGA_EMAIL, KOMGA_EMAIL_PASSWORD
+from config.config import KOMGA_BASE_URL, KOMGA_EMAIL, KOMGA_EMAIL_PASSWORD, KOMGA_LIBRARY_LIST
 import time
 import json
 from tools.log import logger
 import requests
 from requests.adapters import HTTPAdapter
-from threading import Thread
+from threading import Thread, Lock
 import base64
+import queue
 
 # 可配置的订阅事件类型
 RefreshEventType = ["SeriesAdded",
@@ -24,14 +25,16 @@ RefreshEventType = ["SeriesAdded",
 
 
 class KomgaSseClient:
-    def __init__(self, base_url, username, password, api_key=None, timeout=30):
-        self.url = base_url + "/sse/v1/events"
-        self.auth = (username, password)
+    def __init__(self, base_url, username, password, api_key=None, timeout=30, retries=5):
+        self.url = f"{base_url}/sse/v1/events"
+        self.auth = {}
+        self.auth["username"] = username
+        self.auth["password"] = password
         self.running = False
         self.timeout = timeout
         self.thread = None
         self.delay = 1
-        self.max_retries = 5
+        self.max_retries = retries
 
         # 用于Debug的默认回调函数
         self.on_open = lambda: logger.info("成功连接 Komga SSE 服务端点")
@@ -43,9 +46,11 @@ class KomgaSseClient:
             f"Event [{event_type}]: {data}")
 
         self.session = requests.Session()
-        # 传输层自动重连, 重试3次
-        self.session.mount("http://", HTTPAdapter(max_retries=3))
-        self.session.mount("https://", HTTPAdapter(max_retries=3))
+        # 传输层自动重连, 重试 self.max_retries 次
+        self.session.mount(
+            "http://", HTTPAdapter(max_retries=self.max_retries))
+        self.session.mount(
+            "https://", HTTPAdapter(max_retries=self.max_retries))
         self.session.headers.update(
             {
                 # 客户端向服务器表明自身能够接收事件流格式数据的关键标识
@@ -56,13 +61,15 @@ class KomgaSseClient:
                 "Cache-Control": "no-cache",
             }
         )
+        # 使用API_KEY
         if api_key:
             self.session.headers["X-API-Key"] = api_key
             test_url = f"{base_url}/api/v2/users/me"
             response = self.session.get(test_url)
-
+        # 使用账号凭据
         else:
-            credentials = f"{self.auth[0]}:{self.auth[1]}"
+            # 构建认证字符串
+            credentials = f"{self.auth["username"]}:{self.auth["password"]}"
             encoded = base64.b64encode(
                 credentials.encode("utf-8")).decode("utf-8")
             auth_text = f"Basic {encoded}"
@@ -80,17 +87,12 @@ class KomgaSseClient:
                     timeout=self.timeout
                 )
             except Exception as e:
-                logger.error("Komga SSE 连接失败!")
-                self.on_error(str(e))
+                logger.error(
+                    f"Komga SSE 连接失败, HTTP {response.status_code}: {response.reason}")
+                self.on_error(e)
                 exit(1)
-        if response.status_code == 401:
-            logger.error("Komga 身份验证失败!")
-            self.on_error(f"Komga SSE 身份验证失败!")
-            return
         if response.status_code != 200:
-            logger.error("Komga SSE 连接失败!")
-            self.on_error(
-                f"Komga SSE 连接失败, HTTP {response.status_code}: {response.reason}")
+            logger.error("Komga 身份验证失败!")
             return
 
     def start(self):
@@ -185,13 +187,17 @@ class KomgaSseClient:
 
 
 class KomgaSseApi:
-    def __init__(self, base_url=KOMGA_BASE_URL, username=KOMGA_EMAIL, password=KOMGA_EMAIL_PASSWORD, api_key=None, timeout=30):
+    def __init__(self, base_url=KOMGA_BASE_URL, username=KOMGA_EMAIL, password=KOMGA_EMAIL_PASSWORD, api_key=None, timeout=30, retries=5):
         # 实例化 KomgaSseApi 对象
-        self.sse_client = KomgaSseClient(base_url, username, password)
+        self.sse_client = KomgaSseClient(
+            base_url, username, password, api_key, timeout, retries)
         # 绑定回调
         self.sse_client.on_message = self.on_message
         self.sse_client.on_error = self.on_error
         self.sse_client.on_event = self.on_event
+
+        self.series_modified_callbacks = []
+        self.series_callback_lock = Lock()
         # 启动 SSE 监听
         self._start_client()
 
@@ -204,25 +210,59 @@ class KomgaSseApi:
             logger.warning("正在关闭SSE连接...")
             self.sse_client.stop()
 
+    def register_series_update_callback(self, callback):
+        """注册系列更新回调函数"""
+        with self.series_callback_lock:
+            if callback not in self.series_modified_callbacks:
+                self.series_modified_callbacks.append(callback)
+
+    def unregister_series_update_callback(self, callback):
+        """取消注册回调函数"""
+        with self.series_callback_lock:
+            if callback in self.series_modified_callbacks:
+                self.series_modified_callbacks.remove(callback)
+
+    def _notify_callbacks(self, **series_info):
+        """通告所有已注册的回调函数"""
+        with self.series_callback_lock:
+            for callback in self.series_modified_callbacks:
+                try:
+                    callback(series_info)
+                except Exception as e:
+                    logger.error(f"回调函数执行失败: {str(e)}", exc_info=True)
+
     def on_message(self, data):
         try:
             json_data = json.loads(data)
-            # 处理 JSON 数据
         except json.JSONDecodeError as e:
             logger.error("message JSON解析失败:", e)
-        logger.info("普通消息:", json_data)
+        logger.info("收到消息:", json_data)
 
-    # 错误事件回调函数
-    def on_error(self, err):
-        logger.error("错误消息:", err)
+    def on_error(self, e: Exception):
+        """错误事件回调函数"""
+        # 错误处理行为
+        return
 
-    # 订阅事件回调函数
-    def on_event(self, event_type, data):
+    def on_event(self, event_type, event_data):
+        """订阅事件回调函数"""
+        # 仅通知在 RefreshEventType 类型的事件
         if event_type in RefreshEventType:
-            logger.debug(f"捕获订阅事件 [{event_type}]:", data)
-            return {
-                "event_type": event_type,
-                "event_data": data
-            }
+            logger.debug(f"捕获订阅事件 [{event_type}]:", event_data)
+            parsed_data = json.loads(event_data)
+            # 在配置了KOMGA_LIBRARY_LIST时, 不通告 KOMGA_LIBRARY_LIST 外的库更改
+            if parsed_data.get('libraryId') not in KOMGA_LIBRARY_LIST and len(KOMGA_LIBRARY_LIST) > 0:
+                return
+            # 似乎应该使用线程池?
+            notify_thread = Thread(
+                target=self._notify_callbacks,
+                args=(
+                    {
+                        "event_type": event_type,
+                        "event_data": event_data
+                    }
+                ),
+                daemon=True
+            )
+            notify_thread.start()
         else:
-            logger.debug(f"捕获其他事件 [{event_type}]:", data)
+            logger.debug(f"捕获无关事件 [{event_type}]:", event_data)

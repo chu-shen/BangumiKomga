e017b7fde908c486dcabe473eea31cfbc6ca133e
wiige@live.com <wiige@live.com>
2025-05-21 10:51:21 +0800

    feat: 添加`SSEService()`, 适配`USE_BANGUMI_KOMGA_SERVICE_SSE`选项和`refreshMetadataServive.py`

--- refreshMetadataServive.py
+++ refreshMetadataServive.py
@@ -2,11 +2,16 @@ import threading
 import time
 from tools.log import logger
 from config.config import (
-    USE_BANGUMI_KOMGA_SERVICE,
+    USE_BANGUMI_KOMGA_SERVICE_POLL,
+    USE_BANGUMI_KOMGA_SERVICE_SSE,
     SERVICE_POLL_INTERVAL,
     SERVICE_REFRESH_ALL_METADATA_INTERVAL,
+    KOMGA_LIBRARY_LIST
 )
 from refreshMetadata import refresh_metadata, refresh_partial_metadata
+from api.komgaSseApi import KomgaSseApi
+import threading
+import json
 
 
 class PollingCaller:
@@ -43,16 +48,17 @@ class PollingCaller:
         """
         启动服务
         """
-
         def poll():
             while True:
                 try:
+                    # 执行定时轮询
                     if self.refresh_counter >= self.refresh_all_metadata_interval:
                         success = self._safe_refresh(refresh_metadata)
                         self.refresh_counter = 0
                     else:
-                        success = self._safe_refresh(refresh_partial_metadata)
-
+                        success = self._safe_refresh(
+                            refresh_partial_metadata)
+                    # 更新计数器和间隔
                     if not success:
                         retry_delay = min(2**self.interval, 60)
                         time.sleep(retry_delay)
@@ -75,7 +81,7 @@ class PollingCaller:
         threading.Thread(target=poll, daemon=True).start()
 
 
-def main():
+def PollService():
     PollingCaller().start_polling()
 
     # 防止服务主线程退出
@@ -85,8 +91,39 @@ def main():
         logger.warning("服务手动终止: 退出 BangumiKomga 服务")
 
 
+def series_update_sse_handler(data):
+    event_data = json.loads(data["event_data"])
+    recent_modified_series = []
+    # 仅刷新指定 LIBRARY_ID
+    if KOMGA_LIBRARY_LIST and (event_data["libraryId"] in KOMGA_LIBRARY_LIST):
+        recent_modified_series.extend(event_data["seriesId"])
+
+    if recent_modified_series:
+        refresh_metadata(recent_modified_series)
+    else:
+        logger.info("未找到最近添加系列, 无需刷新")
+    return
+
+
+def SSEService():
+    komga_api = KomgaSseApi()
+
+    # 注册回调函数
+    komga_api.register_series_update_callback(series_update_sse_handler)
+    # 主线程保持运行（实际应用中可能有其他逻辑）
+    # 防止服务主线程退出
+    try:
+        threading.Event().wait()
+    except KeyboardInterrupt:
+        # 取消注册
+        komga_api.unregister_series_update_callback(series_update_sse_handler)
+        logger.warning("服务手动终止: 退出 BangumiKomga 服务")
+
+
 if __name__ == "__main__":
-    if USE_BANGUMI_KOMGA_SERVICE:
-        main()
+    if USE_BANGUMI_KOMGA_SERVICE_POLL:
+        PollService()
+    elif USE_BANGUMI_KOMGA_SERVICE_SSE:
+        SSEService()
     else:
         refresh_metadata()

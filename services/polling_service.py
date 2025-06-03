11bee1df28d7c17f68db882764c3e0f17e19b0f2
初神 <45256288+chu-shen@users.noreply.github.com>
2025-06-03 22:14:30 +0800

    refactor: 调整结构

---
+++ services/polling_service.py
@@ -0,0 +1,87 @@
+import threading
+import time
+from tools.log import logger
+from config.config import (
+    BANGUMI_KOMGA_SERVICE_POLL_INTERVAL,
+    BANGUMI_KOMGA_SERVICE_POLL_REFRESH_ALL_METADATA_INTERVAL,
+)
+from refreshMetadata import refresh_metadata, refresh_partial_metadata
+
+
+class PollingCaller:
+    def __init__(self):
+        self.is_refreshing = False
+        self.interval = BANGUMI_KOMGA_SERVICE_POLL_INTERVAL
+        # 多少次轮询后执行一次全量刷新
+        self.refresh_all_metadata_interval = (
+            BANGUMI_KOMGA_SERVICE_POLL_REFRESH_ALL_METADATA_INTERVAL
+        )
+        self.refresh_counter = 0
+        # 添加锁对象
+        self.lock = threading.Lock()
+
+    def _safe_refresh(self, refresh_func):
+        """
+        封装安全刷新操作
+        """
+        with self.lock:
+            if self.is_refreshing:
+                logger.warning("已有元数据刷新任务在运行")
+                return False
+            self.is_refreshing = True
+
+        try:
+            refresh_func()
+            return True
+        except Exception as e:
+            logger.error(f"刷新失败: {str(e)}", exc_info=True)
+            return False
+        finally:
+            with self.lock:
+                self.is_refreshing = False
+
+    def start_polling(self):
+        """
+        启动服务
+        """
+
+        def poll():
+            while True:
+                try:
+                    # 执行定时轮询
+                    if self.refresh_counter >= self.refresh_all_metadata_interval:
+                        success = self._safe_refresh(refresh_metadata)
+                        self.refresh_counter = 0
+                    else:
+                        success = self._safe_refresh(refresh_partial_metadata)
+                    # 更新计数器和间隔
+                    if not success:
+                        retry_delay = min(2**self.interval, 60)
+                        time.sleep(retry_delay)
+
+                    self.refresh_counter += 1
+                    # 等待一个预设时间间隔
+                    time.sleep(self.interval)
+
+                except Exception as e:
+                    logger.error(f"轮询失败: {str(e)}", exc_info=True)
+                    # 指数退避重试
+                    retry_delay = min(2**self.interval, 60)
+                    # 固定值重试
+                    # retry_delay = self.interval
+                    logger.warning(f"{retry_delay}秒后重试...")
+                    time.sleep(retry_delay)
+                    continue
+
+        # 使用守护线程启动轮询
+        threading.Thread(target=poll, daemon=True).start()
+
+
+def poll_service():
+    PollingCaller().start_polling()
+
+    # 防止服务主线程退出
+    try:
+        threading.Event().wait()
+    except KeyboardInterrupt:
+        logger.warning("服务手动终止: 退出 BangumiKomga 服务")
---
+++ services/sse_service.py
@@ -0,0 +1,51 @@
+import threading
+from tools.log import logger
+from config.config import (
+    KOMGA_LIBRARY_LIST,
+)
+from refreshMetadata import refresh_metadata, komga
+from api.komgaSseApi import KomgaSseApi
+import threading
+import json
+
+
+def series_update_sse_handler(data):
+    event_data = json.loads(data["event_data"])
+    series_id = event_data["seriesId"]
+    library_id = event_data["libraryId"]
+    # 筛选有效的 SeriesChanged 事件
+    if data["event_type"] == "SeriesChanged":
+        # 获取指定系列的信息
+        series_detail = komga.getSeries([series_id])
+        # 判断 SeriesChanged 是否为CBL更改
+        for link in series_detail["metadata"]["links"]:
+            if link["label"].lower() == "cbl":
+                continue
+            else:
+                # 无视其他 SeriesChanged 事件
+                return
+    recent_modified_series = []
+    # 仅刷新指定 LIBRARY_ID
+    if KOMGA_LIBRARY_LIST and (library_id in KOMGA_LIBRARY_LIST):
+        recent_modified_series.extend(series_id)
+
+    if recent_modified_series:
+        refresh_metadata(recent_modified_series)
+    else:
+        logger.info("未找到最近添加系列, 无需刷新")
+    return
+
+
+def sse_service():
+    komga_api = KomgaSseApi()
+
+    # 注册回调函数
+    komga_api.register_series_update_callback(series_update_sse_handler)
+    # 主线程保持运行（实际应用中可能有其他逻辑）
+    # 防止服务主线程退出
+    try:
+        threading.Event().wait()
+    except KeyboardInterrupt:
+        # 取消注册
+        komga_api.unregister_series_update_callback(series_update_sse_handler)
+        logger.warning("服务手动终止: 退出 BangumiKomga 服务")

926567450ffa7a4f83687c8c5fc18cddcd156415
wiige@live.com <wiige@live.com>
2025-06-05 05:43:25 +0800

    feat: 为logger添加调试器感知功能, 动态切换日志等级

--- tools/log.py
+++ tools/log.py
@@ -1,6 +1,22 @@
 import sys
+import os
 import logging
 from logging.handlers import RotatingFileHandler
+
+
+def is_in_debug():
+    """检测是否在调试模式下运行"""
+    result = sys.gettrace()
+    print("==============================")
+    print(f"调试器: {result}")
+    print("==============================")
+
+    # 检查调试器钩子, 不为空则是启用了调试
+    if result:
+        return True
+    else:
+        return False
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -15,8 +31,11 @@ fh = RotatingFileHandler(
     encoding="utf-8",
 )
 fh.setFormatter(formatter)
+fh.setLevel(logging.INFO)
 logger.addHandler(fh)
 
 sh = logging.StreamHandler(sys.stdout)
 sh.setFormatter(formatter)
+# 感知调试器, 切换日志等级
+sh.setLevel(logging.DEBUG if is_in_debug() else logging.INFO)
 logger.addHandler(sh)

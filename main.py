ed4c8305533b8094e94ccff1e6b503d7eacab816
wiige@live.com <wiige@live.com>
2025-06-05 15:19:16 +0800

    style: 移除代码

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
 from services.service_runner import run_service
-from tools.log import logger
-import os
-import sqlite3
 
 
 def main():
     run_service()
 
 
-def prepare_procedure():
-    """检查目录权限并提前创建必要目录"""
-    try:
-        # 准备日志目录
-        os.makedirs('/logs', exist_ok=True)
-        # 自动创建db文件
-        with sqlite3.connect('recordsRefreshed.db') as conn:
-            pass
-    except Exception as e:
-        logger.waring(f"环境准备出错: {e}, 请检查目录权限")
-        return
-
-
 if __name__ == "__main__":
-    # 加入启动流程
-    prepare_procedure()
     main()
